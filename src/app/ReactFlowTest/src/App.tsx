3ebeb6cb32740b4412948722e6087481a2cd8ad0
Martin Simecek <msimecek@users.noreply.github.com>
2022-11-10 09:35:46 +0000

    Reactflow test

---
+++ src/app/ReactFlowTest/src/App.tsx
@@ -0,0 +1,11 @@
+import ReactFlow, { useNodesState, useEdgesState, addEdge, Controls, Background, Connection, Node, OnSelectionChangeParams } from "reactflow";
+
+function App() {
+  return (
+    <>
+      <ReactFlow />
+    </>
+  );
+}
+
+export default App;
